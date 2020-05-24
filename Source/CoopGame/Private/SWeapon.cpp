406556bb4648611102565497138fdeb258ab0f38
Shishkacha1 <shishkacha1@gmail.com>
2020-05-24 17:02:14 +0300

    Added damage multipleer depends on flash type

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -26,6 +26,8 @@ ASWeapon::ASWeapon()
 
 	MuzzleSocketName = "MuzzleFlashSocket";
 	TracerTargetName = "Target";
+
+	BaseDamage = 20.0f;
 }
 
 
@@ -60,9 +62,17 @@ void ASWeapon::Fire()
 			// Blocking hit! Process damage
 
 			AActor* HitActor = Hit.GetActor();
-			UGameplayStatics::ApplyPointDamage(HitActor, 20.0f, ShotDirection, Hit, MyOwner->GetInstigatorController(), this, DamageType);
 
 			EPhysicalSurface SurfaceType = UPhysicalMaterial::DetermineSurfaceType(Hit.PhysMaterial.Get());
+
+			float ActualDamage = BaseDamage;
+			if (SurfaceType == SURFACE_FLESHVULNERABLE)
+			{
+				ActualDamage *= 4.0f;
+			}
+
+			UGameplayStatics::ApplyPointDamage(HitActor, ActualDamage, ShotDirection, Hit, MyOwner->GetInstigatorController(), this, DamageType);
+
 
 			UParticleSystem* SelectedEffect = nullptr;
 			switch (SurfaceType)
