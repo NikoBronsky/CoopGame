2ac46aef048b52fcbefa66144655ae3b0bbe3825
Shishkacha1 <shishkacha1@gmail.com>
2020-05-24 18:05:15 +0300

    Created HealthComponent

---
+++ Source/CoopGame/Private/Components/SHealthComponent.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/SHealthComponent.h"
+
+// Sets default values for this component's properties
+USHealthComponent::USHealthComponent()
+{
+	Health = 100;
+}
+
+
+// Called when the game starts
+void USHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
