a37720d51f06803d72d98336003504fe9e9b2a53
Shishkacha1 <shishkacha1@gmail.com>
2020-05-27 12:44:30 +0300

    Added Sound files and 2 sound effects to trackerbot

--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -11,6 +11,7 @@
 #include "Components/SHealthComponent.h"
 #include "SCharacter.h"
 #include "Components/SphereComponent.h"
+#include "Sound/SoundCue.h"
 
 
 // Sets default values
@@ -41,6 +42,7 @@ ASTrackerBot::ASTrackerBot()
 	ExplosionDamage = 40;
 	ExplosionRadius = 200;
 
+	SelfDamageInterval = 0.25f;
 }
 
 // Called when the game starts or when spawned
@@ -115,6 +117,8 @@ void ASTrackerBot::SelfDestruct()
 
 	DrawDebugSphere(GetWorld(), GetActorLocation(), ExplosionRadius, 12, FColor::Red, false, 2.0f, 0, 1.0f);
 
+	UGameplayStatics::PlaySoundAtLocation(this, ExplodeSound, GetActorLocation());
+
 	// Delete Actor immediately
 	Destroy();
 }
@@ -163,9 +167,11 @@ void ASTrackerBot::NotifyActorBeginOverlap(AActor* OtherActor)
 			//We overlapped with a player!
 
 			// Start self destruction sequence
-			GetWorldTimerManager().SetTimer(TimerHandle_SelfDamage, this, &ASTrackerBot::DamageSelf, 0.5f, true, 0.0f);
+			GetWorldTimerManager().SetTimer(TimerHandle_SelfDamage, this, &ASTrackerBot::DamageSelf, SelfDamageInterval, true, 0.0f);
 
 			bStartedSelfDestruction = true;
+
+			UGameplayStatics::SpawnSoundAttached(SelfDestructSound, RootComponent);
 		}
 	}
 
--- enc_temp_folder/95728737d5fd92483eab67e2d75d0a7/STrackerBot.h
+++ enc_temp_folder/95728737d5fd92483eab67e2d75d0a7/STrackerBot.h
@@ -72,19 +72,11 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
 	float ExplosionDamage;
 
-	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
-
-	float SelfDamageInterval;
-
 	FTimerHandle TimerHandle_SelfDamage;
 
 	void DamageSelf();
 
-	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
 	USoundCue* SelfDestructSound;
-
-	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
-	USoundCue* ExplodeSound;
 
 public:	
 	// Called every frame
