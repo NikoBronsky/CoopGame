2ca5448966fd8487f3be8cdf03ec11be2580a31e
Shishkacha1 <shishkacha1@gmail.com>
2020-05-28 11:54:09 +0300

    PowerUp class created

---
+++ Source/CoopGame/Private/SPowerupActor.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPowerupActor.h"
+
+// Sets default values
+ASPowerupActor::ASPowerupActor()
+{
+	PowerupInterval = 0.0f;
+	TotalNumberOfTicks = 0;
+
+
+}
+
+// Called when the game starts or when spawned
+void ASPowerupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+
+}
+
+void ASPowerupActor::OnTickPowerup()
+{
+	TickProcessed++;
+
+	OnPowerupTicked();
+
+	if (TickProcessed >= TotalNumberOfTicks)
+	{
+
+		OnExpired();
+
+		//Delete timer
+		GetWorldTimerManager().ClearTimer(TimerHandle_PowerupTick);
+	}
+}
+
+void ASPowerupActor::ActivatePowerup()
+{
+	if (PowerupInterval > 0.0f)
+	{
+		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true, 0.0f);
+	}
+	else
+	{
+		OnTickPowerup();
+	}
+}
+
