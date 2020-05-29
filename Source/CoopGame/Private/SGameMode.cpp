0e4136ed517835244036bfff6af4103b47225b26
Shishkacha1 <shishkacha1@gmail.com>
2020-05-29 21:55:52 +0300

    Added new lvl. Also added player respawn at new wave

--- Source/CoopGame/Private/SGameMode.cpp
+++ Source/CoopGame/Private/SGameMode.cpp
@@ -41,7 +41,10 @@ void ASGameMode::EndWave()
 void ASGameMode::PrepareForNextWave()
 {
 	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+
 	SetWaveState(EWaveState::WaitingToStart);
+
+	RestartDeadPlayers();
 }
 
 void ASGameMode::CheckWaveState()
@@ -120,6 +123,18 @@ void ASGameMode::SetWaveState(EWaveState NewState)
 	{
 		GS->SetWaveState(NewState);
 	}	
+}
+
+void ASGameMode::RestartDeadPlayers()
+{
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
+	{
+		APlayerController* PC = It->Get();
+		if (PC && PC->GetPawn() == nullptr)
+		{
+			RestartPlayer(PC);
+		}
+	}
 }
 
 void ASGameMode::StartPlay()
